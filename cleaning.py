49896c2bfbcdd7a110cc173fddef5ba54e2b85a4
Elan S Parsons <elansparsons@gmail.com>
2018-10-25 15:53:29 -0400

    begin of preprocessing steps

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,13 @@
+import pandas as pd
+from collections import Counter
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+english_stops = set(stopwords.words('english'))
+from nltk.stem import WordNetLemmatizer
+
 # read in text files
 novels = ['emma.txt','mansfieldpark.txt','northanger.txt','persuasion.txt','pridenp.txt','sensensense.txt']
 inbooks = []
@@ -11,4 +21,35 @@ northanger = inbooks[2]
 persuasion = inbooks[3]
 pridenp = inbooks[4]
 sensensense = inbooks[5]
-# remove 30 lines, punctuation, chapter headers, later split into paragraphs
+
+# EMMA PREPROCESSING
+# remove 30 lines, punctuation, chapter headers, lemmatize
+
+emma.find('VOLUME')
+emma.find('FINIS')
+emma = emma[611:883631]
+
+pattern = r"[A-Za-z]+"
+emtok = nltk.regexp_tokenize(emma, pattern)
+emwords = [w.lower() for w in emtok]
+
+nostops = [w for w in emwords if w not in english_stops]
+
+# lemmatizing
+wnlemma = WordNetLemmatizer()
+emmatized = [wnlemma.lemmatize(w) for w in nostops]
+
+
+# MANSFIELD PARK PREPROCESSING
+
+# NORTHANGER ABBEY PREPROCESSING
+
+# PERSUASION PREPROCESSING
+
+# PRIDE AND PREJUDICE PREPROCESSING
+
+# SENSE AND SENSIBILITY PREPROCESSING
+
+
+
+
